995348ccfce101ac50e20a3c25e1557379db5ef1
hossam_elsayed <hosam.elsaid11@yahoo.com>
2021-06-08 12:14:05 +0200

    add push notification to capacitor

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {
+  ActionPerformed,
+  PushNotificationSchema,
+  PushNotifications,
+  Token
+} from '@capacitor/push-notifications';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
 
   constructor() {}
+
+  ngOnInit(): void {
+    console.log('Initializing HomePage');
+
+    PushNotifications.requestPermissions().then(
+      res => {
+        if (res.receive === 'granted') {
+          PushNotifications.register();
+        } else {
+          console.log('receive NOT granted');
+        }
+      }
+    );
+
+    PushNotifications.addListener('registration',
+    (token: Token)=> {
+      alert('Push registration success, token: '+ token.value);
+    });
+
+    // Some issue with our setup and push will not work
+    PushNotifications.addListener('registrationError',
+      (error: any) => {
+        alert('Error on registration: ' + JSON.stringify(error));
+      }
+    );
+
+    // Show us the notification payload if the app is open on our device
+    PushNotifications.addListener('pushNotificationReceived',
+      (notification: PushNotificationSchema) => {
+        alert('Push received: ' + JSON.stringify(notification));
+      }
+    );
+
+    // Method called when tapping on a notification
+    PushNotifications.addListener('pushNotificationActionPerformed',
+      (notification: ActionPerformed) => {
+        alert('Push action performed: ' + JSON.stringify(notification));
+      }
+    );
+
+  }
 
 }
